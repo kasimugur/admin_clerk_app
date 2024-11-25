2b8de3f60761b6927aacf99a667d85eb7bd18ea4
kasimugur <kasiugur7@gmail.com>
2024-11-25 17:20:06 +0300

    adding categories with prisima #5

--- app/api/categories/[id]/route.ts
+++ app/api/categories/[id]/route.ts
@@ -11,7 +11,26 @@ export async function DELETE(request: NextRequest, { params }: { params: { id: s
         id
       }
     })
-    return  NextResponse.json({message:"success"}, {status:200});
+    return NextResponse.json({ message: "success" }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error: "Failed" }, { status: 500 })
+  }
+}
+
+export async function PUT(request: NextRequest, { params }: { params: { id: string } }) {
+  const { id } = params;
+  const { title, description } = await request.json();
+  try {
+    const updateCategory = await prisma.category.update({
+      where: {
+        id
+      },
+      data: {
+        title,
+        description
+      }
+    })
+    return NextResponse.json(updateCategory, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error: "Failed" }, { status: 500 })
   }
---
+++ components/AddACategory.tsx
@@ -0,0 +1,108 @@
+'use client'
+import React from 'react'
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Button } from './ui/button'
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import axios from 'axios'
+
+const formSchema = z.object({
+  title: z.string().min(2, {
+    message: "title must be at least 2 characters.",
+  }),
+  description: z.string().min(2, {
+    message: "description must be at least 2 characters.",
+  }),
+})
+
+interface AddACategoryProps {
+  fetchCategories: () => void;
+  // onClose:()=>void
+}
+
+export default function AddACategory({ fetchCategories }: AddACategoryProps) {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+      description: "",
+    },
+  })
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    try {
+      await axios.post('/api/categories', { title: values.title, description: values.description })
+      fetchCategories()
+      // onClose();
+    } catch (error) {
+      // const axiosError = error as AxiosError;
+      if (axios.isAxiosError(error)) {
+
+        console.error("onSubmit hatası", error.response ? error.response.data : error.message);
+      } else {
+        console.error("Beklenmeyen hata:", error);
+      }
+    }
+
+  }
+
+  return (
+    <Dialog >
+      <DialogTrigger asChild>
+        <Button variant={'default'}>Add A Category</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Add A New Category</DialogTitle>
+        </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Title</FormLabel>
+                  <FormControl>
+                    <Input   placeholder="title" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Description</FormLabel>
+                  <FormControl>
+                    <Input placeholder="description" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Submit</Button>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  )
+}
